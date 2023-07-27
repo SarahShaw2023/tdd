56b597f41cf603d01e91fd62be6fd89747a36efa
Sarah Shaw <sarah.y.shaw.db@gmail.com>
2023-07-27 20:46:42 +0100

    Added 2 basic tests

---
+++ src/main/java/com/tdd/findhighestnumber/HighestNumberFinder.java
@@ -0,0 +1,17 @@
+package com.tdd.findhighestnumber;
+
+public class HighestNumberFinder {
+    public int findHighestNumber(int[] ary) {
+        return ary[0];
+//        if(ary.length == 0) {
+//            //throw exception
+//            return -1;
+//        } else if (ary.length == 1) {
+//            return ary[0];
+//        }
+//        else {
+//            //Do the generic case
+//            return -1;
+//        }
+    }
+}
---
+++ src/test/java/com/tdd/findhighestnumber/HighestNumberFinderTest.java
@@ -0,0 +1,41 @@
+package com.tdd.findhighestnumber;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HighestNumberFinderTest {
+    //Format: Ary amount + result
+    @Test
+    public void singleItemIsHighest () {
+        //arrange
+        int ary[] = {10};
+        HighestNumberFinder f = new HighestNumberFinder();
+
+        int expectedResult = 10;
+
+        //act
+        int actualResult = f.findHighestNumber(ary);
+
+        //assert
+        assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void firstItemInDoubleAryIsHighest () {
+        //arrange
+        int ary[] = {13, 4};
+        HighestNumberFinder f = new HighestNumberFinder();
+
+        int expectedResult = 13;
+
+        //act
+        int actualResult = f.findHighestNumber(ary);
+
+        //assert
+        assertEquals(actualResult, expectedResult);
+    }
+
+}
+
+
